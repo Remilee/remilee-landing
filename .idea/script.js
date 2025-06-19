fd5568f65af996b7d4ae2e653cfa28807eca1cc1
ekaterina_p <e.polyushina@dis-group.ru>
2025-06-19 17:36:35 +0300

    feat: year calculated with js

--- .idea/script.js
+++ .idea/script.js
@@ -2,4 +2,5 @@ window.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('section').forEach(section => {
         section.style.opacity = 1;
     });
+    document.getElementById('year').textContent = new Date().getFullYear();
 });
